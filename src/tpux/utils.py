0a1f63705188d33471c81e208fa7de2fdff3df74
yixiaoer <shin@yixiaoer.sg>
2024-06-12 15:59:17 +0800

    Read command from stdin
    Co-authored-by: Ayaka <ayx@google.com>

--- src/tpux/utils.py
+++ src/tpux/utils.py
@@ -1,4 +1,7 @@
 import os
+from typing import List
+
+import fabric
 
 def get_config_path() -> str:
     return os.getenv('XDG_CONFIG_HOME') or os.path.expanduser('~/.config')
@@ -6,3 +9,12 @@ def get_config_path() -> str:
 def get_podips_config_file() -> str:
     config_path = get_config_path()
     return os.path.join(config_path, 'tpux', 'podips.txt')
+
+def get_podips() -> List[str]:
+    podips_config_file = get_podips_config_file()
+    with open(podips_config_file, 'r') as f:
+        return [line.rstrip('\n') for line in f]
+
+def run_command(hosts: List[str], command: str) -> None:
+    with fabric.ThreadingGroup(*hosts) as group:
+        group.run(command)
